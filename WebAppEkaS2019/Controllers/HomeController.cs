e4d9d7f68718df5ea44aff9ca534bf8baeed5bc1
olyaapodolyan@gmail.com <olyaapodolyan@gmail.com>
2019-12-13 15:04:37 +0200

    first commit

---
+++ WebAppEkaS2019/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppEkaS2019.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Laskuri()
+        {
+            string apumuuttuja= "";
+            for (int i = 0; i < 10; i++)
+            {
+                apumuuttuja = apumuuttuja + "Laskurin arvo on nyt " + i.ToString();
+            }
+            ViewBag.Message = apumuuttuja;
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAppEkaS2019/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppEkaS2019.Models;
+using System.Web.Mvc;
+
+namespace WebAppEkaS2019.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            northwindEntities db = new northwindEntities();
+            List<Products> tuotteet = db.Products.ToList();
+            db.Dispose();
+            return View(tuotteet);
+        }
+    }
+}
